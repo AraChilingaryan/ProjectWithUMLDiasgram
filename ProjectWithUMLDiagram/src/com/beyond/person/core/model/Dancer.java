ccff933345657b49d83b84dee1dc9c53f3a3eca3
Ara Chilingaryan <chilingaryanara96@gmail.com>
2020-07-31 01:23:47 +0400

    - added 2 directories - added in core/model 4 classes

---
+++ ProjectWithUMLDiagram/src/com/beyond/person/core/model/Dancer.java
@@ -0,0 +1,4 @@
+package com.beyond.person.core.model;
+
+public class Dancer {
+}
---
+++ ProjectWithUMLDiagram/src/com/beyond/person/entrypoint/Main.java
@@ -0,0 +1,7 @@
+package com.beyond.person.entrypoint;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
